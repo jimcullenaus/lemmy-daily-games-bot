155362e2bc51de33af111004a3271f6d16f47a6c
James Cullen <jimcullenaus@gmail.com>
2025-02-05 16:39:55 +1000

    fix: allow retry if screenshot or upload fails

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import LemmyBot from 'lemmy-bot';
 import { config } from 'dotenv';
-import TitleService from './services/title-service';
-import ScreenshotService from './services/screenshot-service';
+import ConnectionsService from './services/connections-service';
 
 let path: string;
 
@@ -27,8 +26,6 @@ const {
     CRON_EXPRESSION
 } = process.env as Record<string, string>;
 
-const _titleService = new TitleService(TIMEZONE);
-const _screenshotService = new ScreenshotService();
 
 const bot = new LemmyBot({
     instance: LOGIN_INSTANCE_NAME,
@@ -42,37 +39,13 @@ const bot = new LemmyBot({
         timezone: TIMEZONE,
         runAtStart: true,
         doTask: async (ref) => {
+            const connectionsService = new ConnectionsService(ref.botActions, TIMEZONE);
             const communityResponse = await ref.botActions.getCommunity({
                 name: `${COMMUNITY_NAME}@${POST_INSTANCE_NAME}`,
             });
 
-            const title = _titleService.getTitle();
-            const screenshot = await _screenshotService.captureScreenshot();
-            console.log('Screenshot returned');
-            let imageUrl : string;
-            try {
-                const image = await ref.botActions.uploadImage(screenshot);
-                if (image.url) {
-                    imageUrl = image.url;
-                } else {
-                    console.log(image);
-                    throw new Error('no url returned from uploading image');
-                }
-            } catch (e) {
-                console.log('error uploading image');
-                console.log(e);
-                throw e;
-            }
+            await connectionsService.postConnectionsDaily(communityResponse.community_view.community.id);
 
-            const body = 'https://www.nytimes.com/games/connections';
-            const createPostResponse = await ref.botActions.createPost({
-                name: title,
-                body: body,
-                community_id: (communityResponse).community_view.community.id,
-                url: imageUrl
-            });
-
-            console.log('created post');
         }
     }
 });
---
+++ src/services/connections-service.ts
@@ -0,0 +1,74 @@
+import { setTimeout } from 'timers/promises'
+import { BotActions } from 'lemmy-bot/dist/types';
+import ScreenshotService from './screenshot-service';
+import TitleService from './title-service';
+
+export default class ConnectionsService {
+
+    private _botActions : BotActions;
+    private _titleService : TitleService;
+    private _screenshotService : ScreenshotService;
+
+    constructor(botActions : BotActions, TIMEZONE : string) {
+        this._botActions = botActions;
+        this._titleService = new TitleService(TIMEZONE);
+        this._screenshotService = new ScreenshotService();
+    }
+
+    public async postConnectionsDaily(communityId : number) {
+        const title = this._titleService.getTitle();
+
+        const screenshot = await this.captureScreenshot();
+
+        // If screenshot was not obtained, make URL blank
+        const imageUrl = screenshot ? await this.uploadImage(screenshot) : '';
+        const body = 'https://www.nytimes.com/games/connections';
+        const createPostResponse = await this._botActions.createPost({
+            name: title,
+            body: body,
+            community_id: communityId,
+            url: imageUrl
+        });
+
+        console.log(`created post: ${createPostResponse.post_view.post.url}`);
+    }
+
+    private async captureScreenshot() : Promise<Buffer | null> {
+        for (let i = 0; i < 5; ++i) {
+            try {
+                return await this._screenshotService.captureScreenshot();
+            } catch (e) {
+                console.log(e);
+            }
+
+            // double delay each time, starting at 1 second
+            const delay = (2 ** i) * 1000;
+            await setTimeout(delay)
+        }
+
+        return null;
+    }
+
+    private async uploadImage(screenshot: Buffer<ArrayBufferLike>) : Promise<string> {
+        for (let i = 0; i < 5; ++i) {
+            try {
+                const image = await this._botActions.uploadImage(screenshot);
+                if (image.url) {
+                    return image.url;
+                } else {
+                    console.log(image);
+                    throw new Error('no url returned from uploading image');
+                }
+            } catch (e) {
+                console.log('error uploading image');
+                console.log(e);
+            }
+
+            // double delay each time, starting at 1 second
+            const delay = (2 ** i) * 1000;
+            await setTimeout(delay)
+        }
+
+        return '';
+    }
+}
